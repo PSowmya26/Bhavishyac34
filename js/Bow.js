a728dca96bf2b361fc33f34b86525f4b2160652a
PSowmya26 <72786302+PSowmya26@users.noreply.github.com>
2022-03-11 12:06:28 +0530

    Update Bow.js

--- js/Bow.js
+++ js/Bow.js
@@ -1,31 +1,23 @@
 class Bow {
-    constructor(x, y, width, height, bowPos, bowAnimation) {
-        
-        this.animation = bowAnimation;
-        this.speed = 0.05;
-        this.body = Bodies.rectangle(x,y,width,height);
-        this.width = width;
-        this.height = height;
+  constructor(x, y, width, height, bowPos) {
+    this.body = Bodies.rectangle(x, y, width, height);
+    this.width = width;
+    this.height = height;
 
-        this.bowPosition = bowPos;
+    this.image = loadImage("./assets/arrow.png");
+    this.bowPosition = bowPos;
+    World.add(world, this.body);
+  }
 
-        World.add(world,this.body);
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
 
-    }
-
-    animate() {
-        this.speed += 0.05;
-    }
-
-    display() {
-        var pos = this.body.position;
-        var index = floor(this.speed % this.animation.length);
-
-        push();
-        translate(pos.x, pos.y);
-        imageMode(CENTER);
-        image(this.animation[index], 0, this.bowPosition, this.width, this.height);
-        noTint();
-        pop();
-    }
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, this.bowPosition, this.width, this.height);
+    pop();
+  }
 }
